9f90ed254febb97020be52beb22f6f0fd4cf85db
Keil <kdh333@nau.edu>
2020-02-19 22:03:48 -0700

    empty-AndroidStudio-skeleton

---
+++ AppPackage/app/src/main/java/com/cs386_s2020/impulsetracker/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.cs386_s2020.impulsetracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
